d989ddb398ca8368275de0221e33688287dca772
crisperi <phoenixedian@gmail.com>
2024-03-21 15:55:18 +0300

    finished

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+
   // Write your code here!
+
+  if(feet <= 400){
+    return 'This one is on me!';
+
+  }else if (feet>400 && feet<= 2000){
+    return 'That will be twenty bucks.'
+  } else if (feet >= 2000 && feet<=2500) { 
+    return 'I will gladly take your thirty bucks.'
+  }else if (feet > 2500){
+    return 'No can do.'
+  };
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(str){
   // Write your code here!
+
+  const cityCondition = str === 'NYC' ? 'Ok, sounds good.' : 'No go.' ;
+  return cityCondition ;
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(value){
   // Write your code here!
+  let message ; 
+  switch(value) {
+    case 'generous' : 
+    message = 'Thank you so much.';
+    break ; 
+    case 'not as generous' :
+      message = 'Thank you.';
+      break;
+    default : 
+    message = 'Bye.';
+  }
+  return message;
 }
